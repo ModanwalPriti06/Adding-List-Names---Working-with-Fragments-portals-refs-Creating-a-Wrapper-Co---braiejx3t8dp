aeb66502b6333fa01e14479858667d38c8c1cb89
Priti Modanwal <99038209+ModanwalPriti06@users.noreply.github.com>
2023-01-11 01:30:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,19 @@ import '../styles/App.css';
 import List from "./List";
 const App = () => {
   //code here
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
+const print=()=>{
+    let data=[...list];
+    let newData={value};
+    data.push(newData);
+    setList(data);
+  
+}
   return (
     <div id="main">
-       <input id="input" onChange={} />
-      <button id="button" onClick={}>Click</button>
+       <input id="input" onChange={(e)=>setValue(e.target.value)}/>
+      <button id="button" onClick={print}>Click</button>
        <ul id="list">
       <List listx={list} />
       </ul>
