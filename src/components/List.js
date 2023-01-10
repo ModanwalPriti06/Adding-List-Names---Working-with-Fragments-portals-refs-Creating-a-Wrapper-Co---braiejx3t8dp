a990366d028f768f5dc8cc34f2e42fc0e4ad73e9
Priti Modanwal <99038209+ModanwalPriti06@users.noreply.github.com>
2023-01-11 01:31:07 +0530

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ListItems from './ListItems';
 //code here
  const List = ({listx}) =>{
-  return(
+   const numbers=listx;
+   return(
+    <>
+    {numbers.map((i) =>
+      <ListItems key={i.toString()}
+                valuex={i.value} />
+    )}
+  </> 
     
- 
-    <ListItems valuex={i} />
-    
-  
   )
 }
 
